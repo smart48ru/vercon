60c050de2d28d721b4e07ea0cd37d6c1bd75a5c3
Stas Kuznetsov <stas@lipsat.ru>
2021-12-26 20:22:16 +0300

    V1.0.0

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/smart48ru/vercon/v2
+module github.com/smart48ru/vercon
 
 go 1.17
--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package vercon
 import "fmt"
 
 func Printer() {
-	fmt.Println("New Version 2.0.0")
+	fmt.Println("New Version 1.0.0")
 }
